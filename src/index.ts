28911f365a43c7b553ed54fb7cb082da77004a95
Balram Chavan <balram.chavan@boehringer-ingelheim.com>
2019-05-01 11:36:25 +0200

    updated source code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-declare var environment: any;
 
+var environment: any;
+export function setupEnvironment(env: any) {
+  environment = env;
+}
 export function Debug() {
   return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
     const method = descriptor.value;
 
-    descriptor.value = function(...args: any) {
+    descriptor.value = function (...args: any) {
       if (environment.production) {
         return;
       }
